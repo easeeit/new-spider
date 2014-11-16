966c38427177d783e505ad178d313f0fc11650f6
easeeit <easeeit@126.com>
2014-11-16 23:34:10 +0800

    home

--- src/main/java/com/dwk/spider/service/UrlService.java
+++ src/main/java/com/dwk/spider/service/UrlService.java
@@ -1,28 +1,51 @@
 package com.dwk.spider.service;
 
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.commons.collections.CollectionUtils;
 
 import com.dwk.spider.util.HttpUtils;
 import com.dwk.spider.util.HttpUtils.Method;
 
 public class UrlService {
+ 
+  private static final String KEY_URL = "url";
+  private static final String KEY_REGEX_LIST = "regexList";
+  private static final String KEY_REGEX_URL = "regexUrl";
+  
+  private List<Map<String, String>> config;
+  
+ 
   
   public void fetch() throws Exception {
-    String content = HttpUtils.call(Method.get, "http://e.games.sina.com.cn/t/news_list/ps4/");
-    // System.out.println(content);
-    content = content.replaceAll("\\r\\n", ""); // 一行
-    Pattern p = Pattern.compile("<ul class=\"hp_newslist\">(.+?)</ul>"); // 1.定位到列表
-    Pattern p2 = Pattern.compile("<li>.*?<a href=\"([^\"]*)\".*?</a>.*?</li>"); // 2.获得列表元素中的文章url
-    Matcher m = p.matcher(content);
-    if (m.find()) {
-      String g = m.group(1);
-      System.out.println(g);
-      Matcher m2 = p2.matcher(g);
-      while (m2.find()) {
-        String url = m2.group(1);
-        System.out.println(url);
+    if (!CollectionUtils.isEmpty(config)) {
+      for (Map<String,String> conf : config) {
+        
+        String content = HttpUtils.call(Method.get, conf.get(KEY_URL));
+        // System.out.println(content);
+        content = content.replaceAll("\\r\\n", ""); // 一行
+        Pattern p = Pattern.compile(conf.get(KEY_REGEX_LIST)); // 1.定位到列表
+        Pattern p2 = Pattern.compile(conf.get(KEY_REGEX_URL)); // 2.获得列表元素中的文章url
+        Matcher m = p.matcher(content);
+        if (m.find()) {
+          String g = m.group(1);
+          System.out.println(g);
+          Matcher m2 = p2.matcher(g);
+          while (m2.find()) {
+            String url = m2.group(1);
+            System.out.println(url);
+          }
+        }
       }
     }
   }
+
+  public void setConfig(List<Map<String, String>> config) {
+    this.config = config;
+  }
+  
+  
 }
